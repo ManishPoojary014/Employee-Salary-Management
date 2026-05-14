bd8a04561a21f25ed173d633f05d9736741803ed
ManishPoojary014 <manishpoojary014@gmail.com>
2026-05-14 08:15:26 +0530

    Implement name validation in AddEmployeeServlet
    Added name validation to ensure only alphabets are accepted.

--- src/main/java/com/servlet/AddEmployeeServlet.java
+++ src/main/java/com/servlet/AddEmployeeServlet.java
@@ -22,7 +22,15 @@ public class AddEmployeeServlet extends HttpServlet {
             String doj = request.getParameter("doj");
             String gender = request.getParameter("gender");
             double salary = Double.parseDouble(request.getParameter("bsalary"));
-
+            
+            //NAME VALIDATION
+            if (!name.matches("^[a-zA-Z\\s]+$")) {
+                out.print("<h2 style='color:red'>Invalid Name! Only alphabets are allowed.</h2>");
+                out.print("<a href='javascript:history.back()'>Go Back</a>");
+                return; // Stops the code here
+            }
+            
+            
             // We pass 0 as the ID, the Database will overwrite it with the next available number
             Employee emp = new Employee(0, name, doj, gender, salary);
 
@@ -31,10 +39,15 @@ public class AddEmployeeServlet extends HttpServlet {
             int result = dao.addEmployee(emp);
 
             if (result > 0) {
+                out.print("<div style='text-align: center; margin-top: 50px;'>");
                 out.print("<h2 style='color:green'>Employee Added Successfully!</h2>");
                 out.print("<a href='index.jsp'>Back to Home</a>");
+                out.print("</div>");
             } else {
+                out.print("<div style='text-align: center; margin-top: 50px;'>");
                 out.print("<h2 style='color:red'>Failed to Add Employee.</h2>");
+                out.print("<a href='javascript:history.back()'>Go Back</a>");
+                out.print("</div>");
             }
         } catch (Exception e) {
             out.print("Error: " + e.getMessage());
